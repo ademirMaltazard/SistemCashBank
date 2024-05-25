ebe08772c9ba4d0b19f25f26ecaa7c1106ffc675
Ademir Gama <159483317+ademirMaltazard@users.noreply.github.com>
2024-05-25 21:41:17 +0000

    criacao das classes Bruxo e Mestiço

--- herança.py
+++ herança.py
@@ -13,3 +13,22 @@ class Trouxa:
         return self.__cpf
 
 # Criação classe Mae que herda atributos da classe Pessoa
+class Bruxo:
+    def __init__(self, casa, patrono):
+        self.casa = casa
+        self.patrono = patrono
+    
+    def lançarFeitico(self):
+        return(f'lançou feitiço: expectro patronom - {self.patrono}')
+
+class Mestico(Trouxa, Bruxo):
+    def __init__(self, nome, cpf, casa, patrono):
+        super().__init__(nome, cpf)
+        Bruxo.__init__(self, casa, patrono)
+
+    def lançarFeitico(self):
+        return(f'{self.nome} lançou feitiço: expectro patronom - {self.patrono}')
+    
+mestico1 = Mestico('Luna Lovegood', '001.001.000.11', 'Grifinoria', 'Coelho')
+
+print(mestico1.lançarFeitico())
