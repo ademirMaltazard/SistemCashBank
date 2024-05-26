bb54a5af8bb4f43e9a0c7813fff76721dff4b837
Ademir Gama <159483317+ademirMaltazard@users.noreply.github.com>
2024-05-26 15:26:58 +0000

    aplicação de polimorfismo e adição de veiculos para teste

--- concessionaria.py
+++ concessionaria.py
@@ -18,6 +18,14 @@ class Carro(Veiculo):
         self.numPortas = numPortas
         self.combustivel = combustivel
     
+    def ExibirInformacoes(self):
+        return (f'Marca: {f"{self.marca}":<10} '
+                f'Modelo: {f"{self.modelo}":<20}'
+                f'Ano de fabricação: {f"{self.anoFab}":<10} '
+                f'Preço: {f"{self.preco}":<10}'
+                f'Numero de Portas: {self.numPortas}    '
+                f'Combustivel: {self.combustivel}')
+    
 class Moto(Veiculo):
     def __init__(self, marca, modelo, anoFab, preco, cilindrada, partida):
         super().__init__(marca, modelo, anoFab, preco)
@@ -52,6 +60,11 @@ moto2 = Moto('honda', 'cb twister', 2012, 22500, 250, 'eletrica')
 moto3 = Moto('yamaha', 'fazer 150s', 2012, 12500, 150, 'eletrica')
 moto4 = Moto('kawasaki', 'Z400', 2024, 34500, 400, 'eletrica')
 moto5 = Moto('bmw', 'GS 300', 2023, 65500, 300, 'eletrica')
+carro1 = Carro('bmw', 'i7', 2024, 1000000, 5, 'eletrico')
+carro2 = Carro('audi', 'A4', 2024, 100000, 5, 'gasolina')
+carro3 = Carro('honda', 'civic', 2024, 1000000, 5, 'flex')
+carro4 = Carro('dodge', 'charger', 2024, 1000000, 5, 'gasolina')
+carro5 = Carro('fiat', 'pulse abath', 2024, 1000000, 5, 'gasolina')
 concessionaria1 = Concessionaria('Dieguinho Veiculos', 'rua arauto, n° 123')
 
 concessionaria1.AddVeiculo(moto1)
@@ -59,4 +72,9 @@ concessionaria1.AddVeiculo(moto2)
 concessionaria1.AddVeiculo(moto3)
 concessionaria1.AddVeiculo(moto4)
 concessionaria1.AddVeiculo(moto5)
+concessionaria1.AddVeiculo(carro1)
+concessionaria1.AddVeiculo(carro2)
+concessionaria1.AddVeiculo(carro3)
+concessionaria1.AddVeiculo(carro4)
+concessionaria1.AddVeiculo(carro5)
 print(concessionaria1.ListarTodos())
